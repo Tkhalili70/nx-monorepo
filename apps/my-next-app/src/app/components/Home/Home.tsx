a3d71f9ac89a3f713c592f650998f0223c3fd975
Tkhalilli70 <khalili.tannaz@gmail.com>
2024-12-31 16:38:27 +0330

    add zod

--- apps/my-next-app/src/app/components/Home/Home.tsx
+++ apps/my-next-app/src/app/components/Home/Home.tsx
@@ -1,6 +1,5 @@
 "use client"
 import { Space, Table, TableProps } from 'antd';
-import { useEffect, useState } from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
 import { useForm } from 'react-hook-form';
@@ -14,7 +13,8 @@ import {
   StyledInput,
   StyledInputWithError, StyledSelect
 } from './styles';
-
+import { z } from 'zod';
+import { zodResolver } from '@hookform/resolvers/zod';
 
 export interface CharacterType {
   key: string;
@@ -22,6 +22,7 @@ export interface CharacterType {
   name: string;
   image: string;
   status: string;
+  gender:string;
   url:string;
   location: {
     name: string;
@@ -40,15 +41,36 @@ interface FormSearchInput {
   name: string;
   status: string;
   species:string;
+  gender:string;
 }
+export type ValidFieldNames =
+  | "name"
+  | "status"
+  | "species"
+  | "gender"
+
 export default function Home() {
   const { characterList, pagination, loading, updatePagination, applyFilters } = useCharacterList();
+  const schema = z.object({
+    name: z.string().min(1, "Name is required"),
+    status: z.string().optional(),
+    species: z.string().optional(),
+    gender: z.string().optional(),
+    // species: z.enum(["Human", "Alien" , "Mythological Creature"], {
+    //   errorMap: () => ({ message: "Gender is invalid. Choose from Male, Female" }),
+    // }).optional(),
+    // gender: z.enum(["Male", "Female"], {
+    //   errorMap: () => ({ message: "Gender is invalid. Choose from Male, Female" }),
+    // }).optional(),
+  });
 
   const {
     register,
     handleSubmit,
     formState: { errors },
-  } = useForm<FormSearchInput>();
+  } = useForm<FormSearchInput>({
+    resolver: zodResolver(schema),
+  });
 
 
 
@@ -60,13 +82,13 @@ export default function Home() {
       render: (text) => {
         return  <Image alt={text} src={text} width={50} height={50}/>
       },
-      width: '20%',
+      width: '15%',
     },
     {
       title: 'Name',
       dataIndex: 'name',
       key: 'name',
-      width: '20%',
+      width: '15%',
     },
     {
       title: 'Status',
@@ -75,18 +97,24 @@ export default function Home() {
       render: (text) => {
         return  <div>{text}</div>
       },
-      width: '20%',
+      width: '15%',
+    },
+    {
+      title: 'Gender',
+      dataIndex: 'gender',
+      key: 'gender',
+      width: '15%',
     },
     {
       title: 'Species',
       dataIndex: 'species',
       key: 'species',
-      width: '20%',
+      width: '15%',
     },
     {
       title: 'Action',
       key: 'action',
-      width: '20%',
+      width: '15%',
       render: (_, record) => (
         <Space size="middle">
           {/*<a>Invite {record.name}</a>*/}
@@ -114,10 +142,11 @@ export default function Home() {
       <StyledForm onSubmit={handleSubmit(handleSubmitBtn)}>
         <StyledInputWithError>
           <StyledInput
-            {...register('name', { required: true })}
+            {...register('name')}
             placeholder="Name"
           />
-          {errors.name && <StyledError>Name is required.</StyledError>}
+          {errors.name && <StyledError>{errors.name.message}</StyledError>}
+
         </StyledInputWithError>
         <StyledInputWithError>
           <StyledSelect {...register('status')}>
@@ -127,9 +156,20 @@ export default function Home() {
             <option value="unknown">unknown</option>
           </StyledSelect>
         </StyledInputWithError>
-
         <StyledInputWithError>
-          <StyledInput {...register('species')} placeholder="Species" />
+          <StyledSelect {...register('gender')}>
+            <option value="">Select gender</option>
+            <option value="Male">Male</option>
+            <option value="Female">Female</option>
+          </StyledSelect>
+        </StyledInputWithError>
+        <StyledInputWithError>
+          <StyledSelect {...register('species')}>
+            <option value="">Select Species</option>
+            <option value="Human">Human</option>
+            <option value="Mythological Creature">Mythological Creature</option>
+            <option value="Alien">Alien</option>
+          </StyledSelect>
         </StyledInputWithError>
         <StyledButtonContainer>
           <StyledButton
@@ -148,7 +188,7 @@ export default function Home() {
         <>
           <Table<CharacterType>
             columns={columns}
-            dataSource={characterList || []}
+            dataSource={characterList}
             loading={loading}
             pagination={pagination}
             onChange={handlePaginationChange}
